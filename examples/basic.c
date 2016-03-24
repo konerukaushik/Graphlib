6a3f2e7ee735413b66211c7462be2faa20ee64c9
Kaushik Koneru <konerukaushik@gmail.com>
2016-03-24 17:37:49 -0400

    Commit Basic Example

---
+++ examples/basic.c
@@ -0,0 +1,59 @@
+/*
+ * In this example we will
+ * try to verify the API's
+ *
+ * Author: Kaushik, Koneru
+ * Email:  konerukaushik@gmail.com
+ */
+
+/*
+ * In this example 
+ * we create a Graph and append few edges
+ * and Find Dijkstra
+ * between the vertices
+ */
+#include <graph.h>
+
+
+int main() {
+  
+  Graph_t              *G;
+  unsigned int          destination;
+  unsigned int          source;
+  unsigned int          weight = 10;
+  bool                  is_directed = FALSE;
+
+  G = Graph_init(10, FALSE);
+
+  source = 0;
+  destination = 1;
+  Graph_add_edge(G, source, destination, weight, is_directed);
+
+  source = 0;
+  destination = 3;
+  weight = 20;
+  Graph_add_edge(G, source, destination, weight, is_directed);
+
+  source = 0;
+  destination = 2;
+  weight = 20;
+  Graph_add_edge(G, source, destination, weight, is_directed);
+
+
+  source = 1;
+  destination = 2;
+  weight = 5;
+  Graph_add_edge(G, source, destination, weight, is_directed);
+
+
+  source = 1;
+  destination = 3;
+  weight = 7;
+  Graph_add_edge(G, source, destination, weight, is_directed);
+
+  printf("----- Dijkstra ----\n");
+
+  Graph_get_dijsktra(G,0);
+
+  return 0;
+}
